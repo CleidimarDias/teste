456ea5c5e2519364306013d9075ce5acffcab5a3
Cleidimar Dias <cleidimarcdsc@gmail.com>
2023-01-31 08:58:53 -0300

    configurado aas paginas

---
+++ src/shared/components/MyForm.js
@@ -0,0 +1,26 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+const MyForm = () => {
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Nome:" variant="outlined" />
+      <TextField id="outlined-basic" label="CPF:" variant="outlined" />
+      <TextField id="outlined-basic" label="Rua ou avenida:" variant="outlined" />
+      <TextField id="outlined-basic" label="Complemento:" variant="outlined" />
+      <TextField id="outlined-basic" label="Cidade:" variant="outlined" />
+      <TextField id="outlined-basic" label="Estado:" variant="outlined" />
+
+      
+      </Box>
+  )
+}
+
+export default MyForm
